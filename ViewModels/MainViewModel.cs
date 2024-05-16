15badf5fd67e157129f9262b1f3feef265410340
PeterIlsted <pibe55630@edu.ucl.dk>
2024-05-16 13:01:17 +0200

    Uge 20, opgave 1 + 2

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using WPFAndMVVM2.Models;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        
+        private PersonRepository personRepo = new PersonRepository();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        // Implement the rest of this MainViewModel class below to 
+        // establish the foundation for data binding !
+        public ObservableCollection<PersonViewModel> PersonVM {  get; set; }
+        private PersonViewModel _selectedPerson;
+        public PersonViewModel selectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                if (value != _selectedPerson)
+                {
+                    _selectedPerson = value;
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+
+        public MainViewModel()
+        {
+            PersonVM = new ObservableCollection<PersonViewModel>();
+            foreach (var person in personRepo.GetAll())
+            {
+                PersonVM.Add(new PersonViewModel(person));
+            }
+
+        }
+        //public void OnPropertyChanged(PersonViewModel personVM)
+        //{
+        //    PropertyChangedEventHandler handler = PropertyChanged;
+        //    if (handler != null) handler(this, new PropertyChangedEventArgs(personVM));
+        //}
+    }
+}
---
+++ ViewModels/PersonViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person person;
+		private string _firstName;
+
+		public string FirstName
+		{
+			get { return _firstName; }
+			set 
+			{ 
+				_firstName = value; 
+				OnPropertyChanged(nameof(FirstName));
+			}
+		}
+		private string _lastName;
+
+		public string LastName
+		{
+			get { return _lastName; }
+			set 
+			{ 
+				_lastName = value;
+				OnPropertyChanged(nameof(LastName));
+			}
+		}
+		private int _age;
+
+		public int Age
+		{
+			get { return _age; }
+			set 
+			{ 
+				_age = value; 
+				OnPropertyChanged(nameof(Age));
+			}
+		}
+		private string _phone;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        //public void OnPropertyChanged(string value = null)
+        //{
+        //    PropertyChangedEventHandler handler = PropertyChanged;
+        //    if (handler != null) handler(this, new PropertyChangedEventArgs(value));
+        //}
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+        public string Phone
+		{
+			get { return _phone; }
+			set 
+			{ 
+				_phone = value;
+				OnPropertyChanged(nameof(Phone));
+			}
+		}
+        public PersonViewModel(Person person)
+        {
+            FirstName = person.FirstName;
+			LastName = person.LastName;
+			Age = person.Age;
+			Phone = person.Phone;
+        }
+        public PersonViewModel() : this(null) { }
+    }
+}
